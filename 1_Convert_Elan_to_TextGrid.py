e80ccd8e26e45cfdc36662244451a60d8c879114
Michel-pierre Jansen <53566590+DeDataDoctor@users.noreply.github.com>
2021-01-30 15:30:39 +0100

    Update 1_Convert_Elan_to_TextGrid.py

--- 1_Convert_Elan_to_TextGrid.py
+++ 1_Convert_Elan_to_TextGrid.py
@@ -16,7 +16,7 @@ from pydub import AudioSegment
 #import wave
 #import contextlib
 
-DirectoryDocument = "PATH\DirectoryList.txt" #this is a list of all the directories, we will use this list to go through the folders we are interested in. We chose to not automatically extract these folders, but to use a short list with folder names since it was only a short list and gives more manual control/can be changed depending on the needs. Another reason to do it this way is because this way we are more aware of choices.
+DirectoryDocument = "PATH\\DirectoryList.txt" #this is a list of all the directories, we will use this list to go through the folders we are interested in. We chose to not automatically extract these folders, but to use a short list with folder names since it was only a short list and gives more manual control/can be changed depending on the needs. Another reason to do it this way is because this way we are more aware of choices.
 
 #Here we define a set of lists that will be cleared each time we run the code, and which will be used in different steps along the document.
 DirectoryList = [] #This is a list we create for all directories we want to search.
@@ -36,12 +36,12 @@ with open(DirectoryDocument) as f:
     for fn in f:
         DirectoryList.append(fn.strip()) #make sure to strip the whitespace and the newline characters
 
-print(DirectoryList) #test, can be commented out
+#print(DirectoryList) #test, can be commented out
 
 
 # searches for all files ending with ".eaf" in the directories (and subdirectories) that we specified in DirectoryList.
 for nr in range(0,len(DirectoryList)):
-    for root, dirs, files in os.walk("C:\\Users\\JansenM\\Documents\\Database AC 2020 complete\\%s" %(DirectoryList[nr])): #Make sure the root here is correct and adjust it when needed.
+    for root, dirs, files in os.walk("PATH\\%s" %(DirectoryList[nr])): #Make sure the root here is correct and adjust it when needed.
         for file in files:
             if file.endswith(File_Extension):
                 OriginalFileAndRootList.append(os.path.join(root, file))
@@ -50,14 +50,14 @@ for nr in range(0,len(DirectoryList)):
                 GeneralRootAndFileList.append(os.path.join(root, file[:-4:]))
 
 #some checks you can activate, check if length stays the same and if name of files remains similar.
-print(OriginalFileAndRootList[1]) 
-print(len(OriginalFileAndRootList))
-print(GeneralRootList[1]) #Check if all file locations and names make sense.
-print(len(GeneralRootList)) #Check this number and make sure it is the same for the other lists, this way we know that we are not losing data during converting.
-print(GeneralFileNameList[1]) 
-print(len(GeneralFileNameList))
-print(GeneralRootAndFileList[1]) 
-print(len(GeneralRootAndFileList))
+# print(OriginalFileAndRootList[1]) 
+# print(len(OriginalFileAndRootList))
+# print(GeneralRootList[1]) #Check if all file locations and names make sense.
+# print(len(GeneralRootList)) #Check this number and make sure it is the same for the other lists, this way we know that we are not losing data during converting.
+# print(GeneralFileNameList[1]) 
+# print(len(GeneralFileNameList))
+# print(GeneralRootAndFileList[1]) 
+# print(len(GeneralRootAndFileList))
 
 #Here we make some string adjustments and then create the audio and TextGrid file lists. One thing you have to be sure of when using this part of the code is that ELAN files have identical names with wav files. 
 #This is convention but not always the case in every database. IMPORTANT: Change the name of the ELAN files and not the other way around if possible. Otherwise you might need to "relocate" audio files when opening the eaf file in ELAN.
@@ -66,24 +66,24 @@ for nr in range(0,len(GeneralRootAndFileList)):
     TextGridFileList.append(GeneralRootAndFileList[nr]+".TextGrid")
 
 #some checks you can activate, check if length stays the same and if name of files remains similar.
-print(AudioFileList[1])
-print(type(AudioFileList[1]))   
-print(len(AudioFileList))
-print(TextGridFileList[1])
-print(type(TextGridFileList[1]))
-print(len(TextGridFileList))
+# print(AudioFileList[1])
+# print(type(AudioFileList[1]))   
+# print(len(AudioFileList))
+# print(TextGridFileList[1])
+# print(type(TextGridFileList[1]))
+# print(len(TextGridFileList))
 
-##Calculate duration in miliseconds for each audio file in the list. We use miliseconds since pympi assumes this as normal input.
+##Calculate duration in miliseconds for each audio file in the list. This step can take some time. We use miliseconds since pympi assumes this as normal input.
 for nr in range(0,len(AudioFileList)):
     audio = AudioSegment.from_file(AudioFileList[nr])
     AudioFileLengthList.append(int(audio.duration_seconds * 1000))
     
 #Here is the final check if all files are still alligned.
-print(AudioFileList[3])
-print(type(AudioFileList[3]))
-print(AudioFileLengthList[3])
-print(type(AudioFileLengthList[3])) 
-print(len(AudioFileLengthList))       
+# print(AudioFileList[3])
+# print(type(AudioFileList[3]))
+# print(AudioFileLengthList[3])
+# print(type(AudioFileLengthList[3])) 
+# print(len(AudioFileLengthList))       
  
 ##iterate over each ELAN file and export to PRAAT format
 for nr in range(0,len(OriginalFileAndRootList)): # nr stands for a number that is part of the itteraction through a string of filename strings in FileList  
@@ -96,5 +96,5 @@ for nr in range(0,len(OriginalFileAndRootList)): # nr stands for a number that i
    
 #Itterate through all the TextGrids and Audio files and copy these to a new location.
 for nr in range(0,len(TextGridFileList)): # nr stands for a number that is part of the itteraction through a string of filename strings in FileList  
-    shutil.copyfile(AudioFileList[nr], "C:\\Users\\JansenM\\Documents\\Database AC 2020 complete\\Scripts for extracting data\\%s%s" %(GeneralFileNameList[nr], ".wav"))
-    shutil.copyfile(TextGridFileList[nr], "C:\\Users\\JansenM\\Documents\\Database AC 2020 complete\\Scripts for extracting data\\%s%s" %(GeneralFileNameList[nr], ".TextGrid"))
+    shutil.copyfile(AudioFileList[nr], "PATH\\%s%s" %(GeneralFileNameList[nr], ".wav"))
+    shutil.copyfile(TextGridFileList[nr], "PATH\\%s%s" %(GeneralFileNameList[nr], ".TextGrid"))
